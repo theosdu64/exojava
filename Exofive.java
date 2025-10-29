fdd46a48341590ba0fb809336576ba9e09dc6d8c
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-29 13:34:27 +0100

    init exo five

---
+++ Exofive.java
@@ -0,0 +1,15 @@
+public class Exofive {
+    public static void main(String[] args) {
+        String charone = "test";
+        String chartwo = "testtwo";
+        String charthree = "test de la phrase";
+
+        String concatstr = charone + chartwo;
+        String concatmethode = charone.concat(chartwo);
+
+        System.out.println("-----------------Premier exo----------------------------");
+        System.out.println(concatstr);
+        System.out.println(concatmethode);
+    }
+    
+}
