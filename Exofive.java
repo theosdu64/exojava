730407963ce9e13d06165571a948ba572eb0c53c
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-29 13:39:35 +0100

    second and third exo done

--- Exofive.java
+++ Exofive.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 public class Exofive {
+    static void search(String str, String phrase) {
+        String strtosearch = str;
+        String phr = phrase;
+
+        System.out.println("Methode search à trouvé");
+        String[] mots = phr.split(" ");
+        System.out.println(Arrays.toString(mots));
+
+        for (String mot : mots) {
+            if (strtosearch.equals(mot)) {
+                System.out.println("Methode search à trouvé");
+                System.out.println(strtosearch);
+                return;
+            }
+        }
+        System.out.println("PAS DE MOT TROUVE");
+    };
+    
     public static void main(String[] args) {
         String charone = "test";
         String chartwo = "testtwo";
@@ -10,6 +30,9 @@ public class Exofive {
         System.out.println("-----------------Premier exo----------------------------");
         System.out.println(concatstr);
         System.out.println(concatmethode);
+        System.out.println("-----------------Deuxieme exo----------------------------");
+        search(charone, charthree);
+
     }
     
 }
