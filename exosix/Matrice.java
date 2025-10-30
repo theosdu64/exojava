f005cb5e128cd558d5097eee1ebca3ed6e7e0e9c
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-30 08:19:48 +0100

    init exosix class and class operation done

---
+++ exosix/Matrice.java
@@ -0,0 +1,5 @@
+package exosix;
+
+public class Matrice {
+
+}
---
+++ exosix/Operation.java
@@ -0,0 +1,18 @@
+package exosix;
+
+public class Operation {
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+    static int divide(int a, int b) {
+        return a / b;
+    }
+    public static void main(String[] args) {
+    }
+}
