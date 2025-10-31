2ae5da9f924eaaa748ce176dbce61ccd571cd61f
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-31 09:25:10 +0100

    make comment done

--- Test.java
+++ Test.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>();
-
+        // parcourt chaque arg de la ligne de commande
         for (String arg : args) {
             String[] numbers = arg.split(" ");
             for (String num : numbers) {
                 Scanner scanner = new Scanner(num);
+                // vérifie si la chaîne est un int valide
                 if (scanner.hasNextInt()) {
                     array.add(Integer.parseInt(num));
                 } else {
@@ -18,6 +19,7 @@ public class Test {
         }
         System.out.println(array);
    
+        // affiche les propriétés de chaque nombre
         for (Integer num : array) {
             String posorneg = (num > 0) ? "positif" : "negatif";
             String pairornot = (num % 2 == 0) ? "pair" : "impair";
