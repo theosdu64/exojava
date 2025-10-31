7ad899100e526f1a5c793e24329d3a0de7fb586c
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-31 09:27:31 +0100

    make comment done

--- School.java
+++ School.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
+/**
+ * gère les notes des étudiants et calcule leurs moyennes
+ */
 public class School {
+    /**
+     * collecte les noms et notes des étudiants puis affiche les moyennes triées
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int notes[] = {};
-
         HashMap<String, Integer> studentName = new HashMap<>();
-
         boolean continueNote = true;
+        // boucle principale de saisie des étudiants
         while (continueNote) {
             System.out.println("Nom");
             String nom = scan.nextLine();
-
             System.out.println("Prenom");
             String prenom = scan.nextLine();
-
             String complete_name = nom + " " + prenom;
-
+            // vérifie si l'étudiant existe déjà
             if(!searchIfPresent(complete_name, studentName)) {
             		continue;
             }
-
             System.out.println("saisir les notes");
             String note = scan.nextLine();
-
             ArrayList<Integer> listeNote = toInt(note);
             int average = average(listeNote);
-
             studentName.put(complete_name, average);
-
             System.out.println("Mr " + nom + " " + prenom + " a une moyenne de " + average);
             System.out.println("Avez vous fini ? (o/n)");
             String path = scan.nextLine();
+            // affiche les résultats triés si terminé
             if (path.equals("o")) {
                 continueNote = false;
                 System.out.println("Ordre decroissant");
@@ -42,11 +42,13 @@ public class School {
             }
         }
     }
-
+    
+    /**
+     * trouve et affiche la note minimale et maximale
+     */
     static void minandmax(int[] notes) {
         int minNote = notes[0];
         int maxNote = notes[0];
-
         for (int note : notes) {
             if (note < minNote) {
                 minNote = note;
@@ -58,7 +60,10 @@ public class School {
         System.out.println("Le mini est de " + minNote);
         System.out.println("Le max est de " + maxNote);
     }
-
+    
+    /**
+     * calcule la moyenne des notes
+     */
     static int average(ArrayList<Integer> notes) {
         int total = 0;
         for (int note : notes) {
@@ -68,19 +73,22 @@ public class School {
         System.out.println("La moyenne est de" + moyenne);
         return moyenne;
     }
-
+    
+    /**
+     * convertit une chaîne de notes séparées par espaces en liste d'entiers
+     */
     static ArrayList<Integer> toInt(String notes) {
         ArrayList<Integer> note = new ArrayList<>();
-
         String[] formatedStr = notes.split(" ");
-
         for (String str : formatedStr) {
             note.add(Integer.parseInt(str));
         }
-
         return note;
     }
-
+    
+    /**
+     * vérifie si un étudiant est déjà enregistré
+     */
     static boolean searchIfPresent(String name, HashMap<String, Integer> liste) {
         if (liste.containsKey(name)) {
             System.out.println(name + " est deja dans la liste");
@@ -92,15 +100,20 @@ public class School {
         }
     }
     
+    /**
+     * trie la map par ordre décroissant des clés
+     */
     static Map<String, Integer> sortedDescMap(HashMap<String, Integer> studentName) {
         TreeMap<String, Integer> sorted = new TreeMap<>(Comparator.reverseOrder());
         sorted.putAll(studentName);
         return sorted;
     }
-
+    
+    /**
+     * trie la map par ordre croissant des clés
+     */
     static Map<String, Integer> sortedCroisMap(HashMap<String, Integer> studentName) {
     		TreeMap<String, Integer> sorted = new TreeMap<>(studentName);
         return sorted;
     }
-
 }
