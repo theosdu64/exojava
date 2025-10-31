721d1293378eb4227741b96fe2b6e42289cf4510
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-31 09:20:22 +0100

    make comment done

--- FTest.java
+++ FTest.java
@@ -4,13 +4,14 @@ public class FTest {
     public static void main(String[] args) {
         int nombre;
         
+        // récupère le nombre depuis les arguments ou depuis l'input
         if(args.length > 0) {
             nombre = Integer.parseInt(args[0]);
         } else {
-
             Scanner sc = new Scanner(System.in);
             System.out.println("Choisissez un entier");
             
+            // valide que l'entrée soit bien un int
             while(!sc.hasNextInt()) {
                 System.out.println("Ce n'est pas un entier valide. Recommencez:");
                 sc.next();
@@ -22,6 +23,7 @@ public class FTest {
         String posorneg;
         String pairornot;
         
+        // détermine si le nombre est positif, négatif ou nul
         if(nombre > 0) {
             posorneg = "positif";
         } else if(nombre < 0) {
@@ -30,6 +32,7 @@ public class FTest {
             posorneg = "nul";
         }
         
+        // détermine si le nombre est pair, impair ou zéro
         if(nombre == 0) {
             pairornot = "égal à zéro";
         } else if(nombre % 2 == 0) {
@@ -37,7 +40,6 @@ public class FTest {
         } else {
             pairornot = "impair";
         }
-
         System.out.println("Le nombre est " + pairornot + " et il est " + posorneg);
     }
 }
