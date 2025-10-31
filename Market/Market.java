c83e5f4b20e7746eb9f3c413c8c3f435c01bf88d
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-31 10:03:06 +0100

    make comment done

--- Market/Market.java
+++ Market/Market.java
@@ -7,17 +7,20 @@ public class Market {
 		Scanner scan = new Scanner(System.in);
 		int menuNbr = 1;
         String[] plats = {"entrée", "plats", "accompagnements", "boissons", "dessert"};
-        
+
         Map<String, ArrayList<String>> ticket = new HashMap<>();
         ArrayList<String> commande = new ArrayList<>();
 
         System.out.println("Combien de menu ?");
         int userInput = scan.nextInt();
-        
+
+		// boucle de création des commandes
 		do {
 			System.out.println("Commande n°" + menuNbr);
+			// parcourt chaque catégorie de plats
 			for(int i = 0; i < plats.length ; i++) {
 				System.out.println("Choix " + plats[i]);
+				// affiche les options disponibles pour cette catégorie
 				for(Integer key : Data.menu[i].keySet()) {
 					System.out.println(key + " - " + Data.menu[i].get(key));
 				}
@@ -32,9 +35,11 @@ public class Market {
 			System.out.println("------------------------------------------------");
 			System.out.println("resume de la dernière commande : " + commande);
 			System.out.println("------------------------------------------------");
+			// enregistre la commande dans le ticket
 			ticket.put("commande n°" + menuNbr, commande);
 			System.out.println("Toute les commandes en cours : ");
 			System.out.println("");
+			// affiche toutes les commandes enregistrées
 			for(Map.Entry<String, ArrayList<String>> entry : ticket.entrySet()) {
 				System.out.println(entry.getKey() + entry.getValue());
 			}
